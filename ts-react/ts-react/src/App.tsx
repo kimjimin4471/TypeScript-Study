b8cf2fc8a6bcb4b8cc739c2325024d355bfd2309
Kimjimin0807 <jm004471@naver.com>
2021-07-28 23:04:40 +0900

    TypeScript Reduce 사용

--- ts-react/ts-react/src/App.tsx
+++ ts-react/ts-react/src/App.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import Greetings from './components/Greetings';
 import './App.css';
 import Counter from './components/Counter';
+import MyForm from './components/MyForm';
+import ReducerSample from './components/ReducerSample';
 
 function App() {
   const onClick = (name: string)=>{
     console.log(`${name} says hello`);
   }
 
+  const onSubmit = (form: {name: string, description: string}) => {
+    console.log(form);
+  }
+
   return (
     <>
-      <Counter/>
+      <ReducerSample />
     </>
   );
 }
